9d4c41d4742673fdbe4bfb1ab6cd76feec9f7f54
furuiyang <furuiyang0715@gmail.com>
2019-07-24 09:56:01 +0800

    完成将多个 csv 文件合并成一个的操作

--- utils.py
+++ utils.py
@@ -2,9 +2,11 @@ import datetime
 import json
 import os
 import subprocess
+import sys
 import time
 
 import pymongo
+import pandas as pd
 
 from config import MONGOURL
 
@@ -27,11 +29,14 @@ def write_codes_to_file(codes):
 def wirte_code_date_to_file():
     # 确定同步的边界时间
     # 开始时间是昨天的零点 结束时间是今天的零点
-    dt1 = datetime.datetime.combine(datetime.date.today() - datetime.timedelta(days=1), datetime.time.min).strftime("%Y-%m-%dT%H:%M:%SZ")
-    dt2 = datetime.datetime.combine(datetime.date.today(), datetime.time.min).strftime("%Y-%m-%dT%H:%M:%SZ")
+    dt1 = datetime.datetime.combine(datetime.date.today() - datetime.timedelta(days=1),
+                                    datetime.time.min).strftime("%Y-%m-%dT%H:%M:%SZ")
+    dt2 = datetime.datetime.combine(datetime.date.today(),
+                                    datetime.time.min).strftime("%Y-%m-%dT%H:%M:%SZ")
 
     # 同步的是昨日的增量数据 文件夹以昨天命名
-    date_int_str = datetime.datetime.combine(datetime.date.today() - datetime.timedelta(days=1), datetime.time.min).strftime("%Y%m%d")
+    date_int_str = datetime.datetime.combine(datetime.date.today() - datetime.timedelta(days=1),
+                                             datetime.time.min).strftime("%Y%m%d")
 
     file_path = os.path.join(os.getcwd(), "exportdir/" + date_int_str)
     os.makedirs(file_path, exist_ok=True)
@@ -47,6 +52,47 @@ def wirte_code_date_to_file():
         # print(command)
         log_file = open("export_log.log", "a+")
         subprocess.call(command, shell=True, stderr=log_file)
+
+
+def merge_csv(Folder_Path, SaveFile_Path, SaveFile_Name):
+    """
+    将多个 csv 文件合并成一个
+    :param Folder_Path: 需要拼接的文件夹的完整路径
+    :param SaveFile_Path: 拼接后要保存到的文件夹路劲
+    :param SaveFile_Name: 拼接后要保存的文件名
+    :return:
+    """
+    # 修改当前工作目录
+    os.chdir(Folder_Path)
+    # 将该文件夹下的所有文件名存入一个列表
+    file_list = os.listdir()
+
+    # 读取第一个CSV文件并包含表头
+    df = pd.read_csv(os.path.join(Folder_Path, file_list[0]))
+
+    # 创建要保存的文件夹
+    os.makedirs(SaveFile_Path, exist_ok=True)
+
+    # 将读取的第一个CSV文件写入合并后的文件保存
+    save_file = os.path.join(SaveFile_Path, SaveFile_Name)
+    df.to_csv(save_file)
+
+    # 循环遍历列表中各个CSV文件名，并追加到合并后的文件
+    # count = 0
+    try:
+        for i in range(1, len(file_list)):
+            # print(os.path.join(Folder_Path, file_list[i]))
+            df = pd.read_csv(os.path.join(Folder_Path, file_list[i]))
+            # print(df)
+            # print(df.shape[0])
+            # count += df.shape[0]
+
+            # print()
+            # print()
+
+            df.to_csv(save_file, encoding="utf-8", index=False, header=False, mode='a+')
+    except Exception:
+        pass
 
 
 if __name__ == "__main__":
