773fa4d7bd3e6111df173bab9d22aad69c418bf8
furuiyang <furuiyang0715@gmail.com>
2019-07-24 14:30:23 +0800

    code add

--- config.py
+++ config.py
@@ -4,3 +4,10 @@ env = os.environ.get
 
 
 MONGOURL = env("MONGOURL", "mongodb://127.0.0.1:27017")
+
+MYSQLHOST = env("MYSQLHOST", "localhost")
+MYSQLUSER = env("MYSQLUSER", "root")
+MYSQLPASSWORD = env("MYSQLPASSWORD", 'ruiyang')
+MYSQLPORT = env("MYSQLPORT", 3306)
+MYSQLDB = env("MYSQLDB", "test01")
+MYSQLTABLE = env("MYSQLTABLE", 'inc_mins')
